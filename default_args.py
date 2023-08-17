b681b159ab586f9c4fcbc162bd0e02daf4216bd6
Violet Xiang <ziyxiang@node2-ccn2cluster.stanford.edu>
2023-08-17 12:48:21 -0700

    add collect rewards exps

--- default_args.py
+++ default_args.py
@@ -12,16 +12,16 @@ class DefaultGlobalArgs:
     buffer_size: int = 20_000
     lr: float = 1e-4
     gamma: float = 0.99    # discount factor
-    epoch: int = 1    #1000
-    step_per_epoch: int = 50  #5000
-    step_per_collect: int = 20  #2000
+    epoch: int = 100
+    step_per_epoch: int = 5000
+    step_per_collect: int = 2000
     repeat_per_collect: int = 10
     batch_size: int = 2048
     # number of train and test envs in the vectorized environment
     train_env_num: int = 10
     test_env_num: int = 2
     # number of episode used to evaluate trained agents after training is finished
-    eval_eps: int = 2
+    eval_eps: int = 3
     logdir: str = 'log'
     device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
     # ckpt_dir can be provided when continue to train or only doing evaluation
