e66d54fed8a2ad7b208054fd2cabc4e0255e5428
violetxi <ziyxiang@stanford.edu>
2023-09-06 10:58:07 -0700

    minor

--- social_rl/policy/social_influence_policy.py
+++ social_rl/policy/social_influence_policy.py
@@ -145,7 +145,7 @@ class SocialInfluencePolicy(BasePolicy):
         # state reset happens for every batch
         moa_loss = self.model(curr_obs, prev_act, other_act) * self.lr_scale
         moa_loss.backward()
-        self.optim.step()
+        self.model_optimizer.step()
         res.update(
             {"moa_loss": moa_loss.item()}
         )
