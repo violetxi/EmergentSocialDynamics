fix: svo radians & reset hidden state correctly

social_rl/model/core.py
@@ -72,31 +72,54 @@ class ConvGRU(nn.Module):
         self.fc = nn.Linear(config['rnn_hidden_size'], config['output_dim'])
         self.dist_mean = nn.Linear(config['output_dim'], config['output_dim'])
         self.dist_std = nn.Linear(config['output_dim'], config['output_dim'])
+        # to keep track of its hidden state
+        self.state = None
 
-    def forward(self, obs, state=None, info={}):        
+    def forward(self, obs, state=None):
+        # state is the initial hidden state
         obs = obs.observation.curr_obs.cuda(non_blocking=True)    
         # stacked inputs assuming images are grayscaled        
         bs, ts, c, h, w = obs.shape
         processed_obs = []
-        for i in range(ts):            
+        for i in range(ts):       
             processed_obs.append(self.encoder(obs[:, i, :, :, :]))            
         processed_obs = torch.stack(processed_obs).permute(1, 0, 2)
-        if state is None:
-            state = torch.zeros((
-                self.gru.num_layers, bs, self.gru.hidden_size
-                )).cuda()        
-        out, state = self.gru(processed_obs, state)
+        # during batch training, uses reset state every time
+        if bs > 50:
+            if state is None:
+                state = torch.zeros((
+                    self.gru.num_layers, bs, self.gru.hidden_size
+                    )).cuda()
+            out, state = self.gru(processed_obs, state)
+        else:
+            # during data collection, use state with past history
+            if self.state is None:                
+                self.state = torch.zeros((
+                    self.gru.num_layers, bs, self.gru.hidden_size
+                    )).cuda()
+            out, self.state = self.gru(processed_obs, self.state)
+
+        # else:
+        #     if self.state.shape[1] != bs:
+        #         state = torch.zeros((
+        #             self.gru.num_layers, bs, self.gru.hidden_size
+        #             )).cuda()
+        #         out, state = self.gru(processed_obs, state)
+        #     else:
+        #         out, state = self.gru(processed_obs, self.state)
+        # self.state = state.detach()
         last_out = out[:, -1, :]
         logits = self.fc(last_out)
-        return logits, state
+        return logits, self.state
     
 
 class ConvGRUICM(ConvGRU):
     def __init__(self, config):
         super(ConvGRUICM, self).__init__(config)        
 
-    # override forward method such that there is no state returned
+    # override forward method such that there is no state returned    
     def forward(self, obs, state=None, info={}):
+        # state is the initial hidden state
         if isinstance(obs, torch.Tensor):
             obs = obs.cuda()
         else:
@@ -107,11 +130,16 @@ class ConvGRUICM(ConvGRU):
         for i in range(ts):
             processed_obs.append(self.encoder(obs[:, i, :, :, :]))
         processed_obs = torch.stack(processed_obs).permute(1, 0, 2)
-        if state is None:
-            state = torch.zeros((
-                self.gru.num_layers, bs, self.gru.hidden_size
-                )).cuda()        
-        out, state = self.gru(processed_obs, state)
+            
+        if self.state is None:
+            if state is None:
+                state = torch.zeros((
+                    self.gru.num_layers, bs, self.gru.hidden_size
+                    )).cuda()
+            out, state = self.gru(processed_obs, state)
+        else:
+            out, state = self.gru(processed_obs, self.state)
+        self.state = state
         last_out = out[:, -1, :]
         logits = self.fc(last_out)
         return logits

social_rl/policy/svo_policy.py
@@ -88,8 +88,8 @@ class SVOPolicy(BasePolicy):
         # compute current SVO
         epsilon = 1e-10
         mean_other_rew = batch.obs.observation.other_rews.mean(axis=1)
-        svo_hat = np.degrees(np.arctan2(mean_other_rew, batch.rew + epsilon))
-        svo_hat = np.clip(svo_hat, 0, 90)
+        svo_hat = np.arctan2(mean_other_rew, batch.rew + epsilon)
+        svo_hat = np.clip(svo_hat, 0, np.pi/2)
         self.mean_svo_hat = svo_hat.mean()
         intr_rew = np.abs(svo_hat - self.svo) * self.reward_scale        
         batch.policy = Batch(
@@ -98,6 +98,8 @@ class SVOPolicy(BasePolicy):
             )
         # add intr_rew and reward loss to reward
         batch.rew -= intr_rew
+        # reset state for the inner policy
+        self.policy.actor.preprocess.state = None
         return self.policy.process_fn(batch, buffer, indices)
 
     def post_process_fn(

social_rl/tianshou_elign/data/collector.py
@@ -180,6 +180,13 @@ class Collector(object):
 
     def _reset_state(self, id: Union[int, List[int]]) -> None:
         """Reset the hidden state: self.data.state[id]."""
+        for agent_id, policy in self.policy.policies.items():
+            if hasattr(policy, "actor"):                
+                # PPO base policy 
+                policy.actor.preprocess.state = None        
+            else:
+                # other IM policies wrapped PPO
+                policy.policy.actor.preprocess.state = None
         if hasattr(self.data.policy, "hidden_state"):
             state = self.data.policy.hidden_state  # it is a reference
             if isinstance(state, torch.Tensor):
@@ -311,12 +318,8 @@ class Collector(object):
             else:
                 if no_grad:
                     with torch.no_grad():  # faster than retain_grad version
-                        # self.data.obs will be used by agent to get result                        
-                        cpu_percent_start = psutil.cpu_percent(interval=None)
-                        result = self.policy(self.data, last_state)
-                        cpu_percent_end = psutil.cpu_percent(interval=None)
-                        avg_cpu = (cpu_percent_start + cpu_percent_end) / 2
-                        #print(f"Average CPU usage during self.policy: {avg_cpu}")
+                        # self.data.obs will be used by agent to get result
+                        result = self.policy(self.data, last_state)                        
                 else:
                     result = self.policy(self.data, last_state)
                 # update state / policy into self.data
@@ -325,8 +328,7 @@ class Collector(object):
                 state = result.get("state", None)
                 if state is not None:
                     policy.hidden_state = state  # save state into buffer
-                # update action for multi-agent into self.data 
-                
+                # update action for multi-agent into self.data                 
                 act = []
                 for k, v in result.items():
                     act.append(v.act)